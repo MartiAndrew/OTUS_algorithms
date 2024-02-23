50fe8cc1e360ff7f4d1be4573f5cb7d0a41a51ff
Andrew Mishkov <amishkov@ritm.media>
2024-02-23 23:09:58 +0300

    added hw10 hw19

---
+++ otus_algorithms/homework_10.py
@@ -0,0 +1,31 @@
+class Node:
+    def __init__(self, key):
+        self.left = None
+        self.right = None
+        self.val = key
+
+def insert(root, key):
+    if root is None:
+        return Node(key)
+    else:
+        if root.val < key:
+            root.right = insert(root.right, key)
+        else:
+            root.left = insert(root.left, key)
+    return root
+
+def inorder_traversal(root):
+    if root:
+        inorder_traversal(root.left)
+        print(root.val, end=' ')
+        inorder_traversal(root.right)
+
+# Пример использования:
+root = None
+keys = [5, 3, 7, 1, 4, 6, 8]
+
+for key in keys:
+    root = insert(root, key)
+
+print("Inorder traversal:")
+inorder_traversal(root)
---
+++ otus_algorithms/homework_19.py
@@ -0,0 +1,43 @@
+import  doctest
+
+def demucron_algorithm(A):
+    """
+    алгоритм Демюкрона
+    :param A: вектор смежности в виде массива
+    :return: результат сортировки
+    >>> demucron_algorithm([[1, 2], [2, 3], [], [4], [3]])
+    [[0], [1], [2]]
+    """
+    N = len(A)
+
+    in_degree = [0] * N
+    queue = []
+    result = []
+
+    for i in range(N):
+        for j in A[i]:
+            in_degree[j] += 1
+
+    for i in range(N):
+        if in_degree[i] == 0:
+            queue.append(i)
+
+    while queue:
+        level = []
+        for _ in range(len(queue)):
+            vertex = queue.pop(0)
+            level.append(vertex)
+
+            for neighbor in A[vertex]:
+                in_degree[
+                    neighbor] -= 1
+                if in_degree[neighbor] == 0:
+                    queue.append(
+                        neighbor)
+
+        result.append(level)
+    return result
+
+
+if __name__ == '__main__':
+    doctest.testmod(verbose=True)
